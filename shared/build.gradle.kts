1f19256ab8edc8128928a44f02663b5e7cb8fb79
Vladimir Vetrov <pdastore1@gmail.com>
2024-06-08 22:16:24 +0300

    Remove android target; Add jvm target

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,19 +1,10 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-    
+    jvm()
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,17 +20,5 @@ kotlin {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
         }
-    }
-}
-
-android {
-    namespace = "com.alongo.whm.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
