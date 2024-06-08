cfd6514b97728a1e7f5ca1cbb4d3ebcc020bd9e3
Vladimir Vetrov <pdastore1@gmail.com>
2024-06-09 00:36:27 +0300

    Fix module dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -19,49 +19,12 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
-        jvmMain {
-            dependsOn(commonMain)
-        }
-
-        // region Apple main targets
-        val appleMain by getting {
-            dependsOn(commonMain)
-        }
-
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val macosX64Main by getting
-        val macosArm64Main by getting
-
-        iosMain {
-            dependsOn(appleMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-            macosX64Main.dependsOn(this)
-            macosArm64Main.dependsOn(this)
-        }
-        macosMain {
-            dependsOn(appleMain)
-        }
-        // endregion
-
         val commonTest by getting {
-            dependsOn(commonMain)
             dependencies {
                 implementation(kotlin("test"))
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
-        }
-        appleTest {
-            dependsOn(commonTest)
-            dependsOn(appleMain)
-        }
-        jvmTest {
-            dependsOn(commonTest)
         }
     }
 }
