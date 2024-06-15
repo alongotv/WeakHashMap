d20ee7d693482c40c71de8932679b4794a331b28
alongo <pdastore1@gmail.com>
2024-06-15 14:18:18 +0300

    Add WeakHashMapTest for jvm

---
+++ shared/src/jvmTest/kotlin/WeakHashMapTest.kt
@@ -0,0 +1,23 @@
+import kotlinx.coroutines.runBlocking
+import java.lang.ref.WeakReference
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class WeakHashMapTest {
+
+    private val weakHashMap = WeakHashMap<IntContainer, Any>()
+
+    @Test
+    fun `map is empty after the only key is deallocated`() = runBlocking {
+
+        val weak = WeakReference(IntContainer(0))
+
+        weak.get()?.let {
+            weakHashMap[it] = Any()
+        }
+        System.gc()
+        Thread.sleep(1000)
+
+        assertEquals(0, weakHashMap.keys.size)
+    }
+}
